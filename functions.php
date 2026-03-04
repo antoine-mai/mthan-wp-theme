d409b2e0f39db09e1ed32c256c543d4dac89f835
mthan.net <mthandotnet@gmail.com>
2026-03-04 18:12:46 +0700

    Wakanda Forever!

--- functions.php
+++ functions.php
@@ -1,7 +1,12 @@
-<?php
+<?php defined ('ABSPATH') || exit;
+/**
+ * 
+**/
+define ('MTHAN_THEME_DIR', get_template_directory());
+define ('MTHAN_THEME_VERSION', '1.0.0');
 // Theme setup
-if ( ! function_exists( 'mytheme_setup' ) ) {
-    function mytheme_setup() {
+if ( ! function_exists( 'mthan_setup' ) ) {
+    function mthan_setup() {
         add_theme_support( 'title-tag' );
         add_theme_support( 'post-thumbnails' );
         add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
@@ -11,12 +16,12 @@ if ( ! function_exists( 'mytheme_setup' ) ) {
         ) );
     }
 }
-add_action( 'after_setup_theme', 'mytheme_setup' );
+add_action( 'after_setup_theme', 'mthan_setup' );
 
 // Enqueue styles and scripts
-function mytheme_enqueue_assets() {
-    wp_enqueue_style( 'mytheme-style', get_stylesheet_uri(), array(), wp_get_theme()->get( 'Version' ) );
-    wp_enqueue_style( 'mytheme-main', get_template_directory_uri() . '/assets/css/main.css', array(), '1.0' );
-    wp_enqueue_script( 'mytheme-script', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), '1.0', true );
+function mthan_enqueue_assets() {
+    wp_enqueue_style( 'mthan-style', get_stylesheet_uri(), array(), wp_get_theme()->get( 'Version' ) );
+    wp_enqueue_style( 'mthan-main', get_template_directory_uri() . '/assets/css/main.css', array(), '1.0' );
+    wp_enqueue_script( 'mthan-script', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), '1.0', true );
 }
-add_action( 'wp_enqueue_scripts', 'mytheme_enqueue_assets' );
+add_action( 'wp_enqueue_scripts', 'mthan_enqueue_assets' );
