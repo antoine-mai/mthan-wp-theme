317436e99243e902d6033b12b8f4cf00cfa87599
mthan.net <mthandotnet@gmail.com>
2026-03-05 02:50:23 +0700

    Wakanda Forever!

--- admin/footer.php
+++ admin/footer.php
@@ -6,6 +6,17 @@ CSF::createSection($prefix, array(
     'icon' => 'fas fa-arrow-down',
     'fields' => array(
             array(
+            'id' => 'footer_layout',
+            'type' => 'select',
+            'title' => 'Footer Layout',
+            'desc' => 'Select the footer style for your site.',
+            'options' => array(
+                'style-1' => 'Style 1 - Default Footer',
+                'style-2' => 'Style 2 - Alternative Footer',
+            ),
+            'default' => 'style-1',
+        ),
+            array(
             'id' => 'footer_text',
             'type' => 'wp_editor',
             'title' => 'Copyright Text',
--- admin/header.php
+++ admin/header.php
@@ -6,6 +6,24 @@ CSF::createSection($prefix, array(
     'icon' => 'fas fa-arrow-up',
     'fields' => array(
             array(
+            'id' => 'header_layout',
+            'type' => 'select',
+            'title' => 'Header Layout',
+            'desc' => 'Select the header style for your site.',
+            'options' => array(
+                'style-1' => 'Style 1 - Default Header',
+                'style-2' => 'Style 2 - Alternative Header',
+            ),
+            'default' => 'style-1',
+        ),
+            array(
+            'id' => 'header_topbar',
+            'type' => 'switcher',
+            'title' => 'Enable Topbar',
+            'desc' => 'Show or hide the top bar above the main header.',
+            'default' => true,
+        ),
+            array(
             'id' => 'header_sticky',
             'type' => 'switcher',
             'title' => 'Sticky Header',
