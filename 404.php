d5020b45aca2d4f3848e063e3c6c19ac69ca8413
mthan.net <mthandotnet@gmail.com>
2026-03-05 13:06:05 +0700

    Wakanda Forever!

--- 404.php
+++ 404.php
@@ -1,9 +1,22 @@
 <?php defined('ABSPATH') or die('Cheatin\' uh?');
 get_header(); ?>
 
-<main id="site-content">
-    <h1><?php esc_html_e( 'Page not found', 'my-theme' ); ?></h1>
-    <p><?php esc_html_e( 'Sorry, but the page you were trying to view does not exist.', 'my-theme' ); ?></p>
-</main>
+<section class="error-section">
+    <div class="image-layer" style="background-image: url(<?php echo esc_url(get_template_directory_uri() . '/assets/images/background/bg-404.jpg'); ?>);"></div>
+    <div class="auto-container">
+        <div class="error-image">
+            <img src="<?php echo esc_url(get_template_directory_uri() . '/assets/images/resource/404-image.png'); ?>" alt="" title="">
+        </div>
+        <div class="content-box">
+            <h4>Oops! Page Not Found.</h4>
+            <div class="text">It looks like nothing was found at this location. Maybe try one of the links below or a search?</div>
+            <div class="link-box">
+                <a href="<?php echo esc_url(home_url('/')); ?>" class="theme-btn btn-style-four">
+                    <span class="btn-title">Back to Home <i class="arrow flaticon-play-button-1"></i></span>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php get_footer(); ?>
