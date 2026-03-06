bcfc637e9a442ed516e52808ad085e74fd4ceaa3
mthan.net <mthandotnet@gmail.com>
2026-03-06 17:36:02 +0700

    rename to Sections, add global section defaults along with toggles

--- admin/sections.php
+++ admin/sections.php
@@ -1,6 +1,35 @@
 <?php defined('ABSPATH') or die('Cheatin\' uh?');
 // Sections Settings
-$section_fields = array();
+
+$section_fields = [
+    [
+        'type'    => 'subheading',
+        'content' => 'Global Section Settings',
+    ],
+    [
+        'id'      => 'default_section_padding_top',
+        'type'    => 'text',
+        'title'   => 'Default Section Top Padding',
+        'default' => '120px',
+    ],
+    [
+        'id'      => 'default_section_padding_bottom',
+        'type'    => 'text',
+        'title'   => 'Default Section Bottom Padding',
+        'default' => '120px',
+    ],
+    [
+        'id'      => 'default_section_bg',
+        'type'    => 'color',
+        'title'   => 'Default Section Background Color',
+        'default' => '#ffffff',
+    ],
+    [
+        'type'    => 'subheading',
+        'content' => 'Enable / Disable Sections',
+    ],
+];
+
 $sections_path = get_template_directory() . '/sections/';
 
 if (is_dir($sections_path)) {
@@ -8,24 +37,24 @@ if (is_dir($sections_path)) {
     if ($files) {
         foreach ($files as $file) {
             $filename = basename($file, '.php');
-            $section_fields[] = array(
-                'id' => 'enable_section_' . str_replace('-', '_', $filename),
-                'type' => 'switcher',
-                'title' => 'Enable ' . ucwords(str_replace('-', ' ', $filename)) . ' Section',
+            $section_fields[] = [
+                'id'      => 'enable_section_' . str_replace('-', '_', $filename),
+                'type'    => 'switcher',
+                'title'   => 'Enable ' . ucwords(str_replace('-', ' ', $filename)),
                 'default' => true,
-            );
+            ];
         }
     }
 }
 
-CSF::createSection(MTHAN_THEME_OPTIONS, array(
-    'id' => 'sections_toggle',
-    'title' => 'Sections Toggle',
-    'icon' => 'fas fa-toggle-on',
-    'fields' => empty($section_fields) ? array(
-            array(
-            'type' => 'content',
+CSF::createSection(MTHAN_THEME_OPTIONS, [
+    'id'     => 'sections_settings',
+    'title'  => 'Sections',
+    'icon'   => 'fas fa-layer-group',
+    'fields' => empty($files) ? [
+        [
+            'type'    => 'content',
             'content' => 'No sections found in the /sections/ directory.',
-        )
-    ) : $section_fields
-));
+        ]
+    ] : $section_fields
+]);
