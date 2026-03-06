42c57223c8725d53432b1421d23db00598c3c5b4
mthan.net <mthandotnet@gmail.com>
2026-03-06 12:57:05 +0700

    convert all array() to [] short syntax in admin and sections

--- sections/myaccount.php
+++ sections/myaccount.php
@@ -1,25 +1,25 @@
 <?php defined('ABSPATH') or die('Cheatin\' uh?');
 // Blog Settings
-CSF::createSection($prefix, array(
+CSF::createSection($prefix, [
     'id' => 'blog_settings',
     'title' => 'Blog',
     'icon' => 'fas fa-edit',
-    'fields' => array(
-            array(
+    'fields' => [
+            [
             'id' => 'blog_layout',
             'type' => 'select',
             'title' => 'Blog Layout',
-            'options' => array(
+            'options' => [
                 'list' => 'List Layout',
                 'grid' => 'Grid Layout',
-            ),
+            ],
             'default' => 'list'
-        ),
-            array(
+        ],
+            [
             'id' => 'blog_sidebar',
             'type' => 'switcher',
             'title' => 'Enable Sidebar on Single Post',
             'default' => true,
-        ),
-    )
-));
+        ],
+    ]
+]);
