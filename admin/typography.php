0ef2ddef0fcd1f8a6f9a09087b424265509041af
mthan.net <mthandotnet@gmail.com>
2026-03-06 17:28:23 +0700

    set default typography fonts

--- admin/typography.php
+++ admin/typography.php
@@ -1,22 +1,30 @@
 <?php defined('ABSPATH') or die('Cheatin\' uh?');
 
 // Typography Settings
-CSF::createSection(MTHAN_THEME_OPTIONS, array(
+CSF::createSection(MTHAN_THEME_OPTIONS, [
     'id' => 'typography_settings',
     'title' => 'Typography',
     'icon' => 'fas fa-font',
-    'fields' => array(
-        array(
+    'fields' => [
+        [
             'id' => 'body_typography',
             'type' => 'typography',
             'title' => 'Body Font',
             'output' => 'body, p',
-        ),
-        array(
+            'default' => [
+                'font-family' => 'Inter',
+                'type'        => 'google',
+            ],
+        ],
+        [
             'id' => 'heading_typography',
             'type' => 'typography',
             'title' => 'Heading Font (H1 - H6)',
             'output' => 'h1, h2, h3, h4, h5, h6, .theme-btn',
-        ),
-    )
-));
+            'default' => [
+                'font-family' => 'Libre Baskerville',
+                'type'        => 'google',
+            ],
+        ],
+    ]
+]);
